53eaabf1762058cc15f21975fba20ed73703686f
Aman Singh <159300622+Aman918an@users.noreply.github.com>
2025-10-13 03:51:46 +0530

    Update top.sv
    Code has some bug
    Issue: Unknown

--- src/top.sv
+++ src/top.sv
@@ -1,23 +1,6 @@
 // using harvard arch in this processor
-
-
 `timescale 1ns / 1ps
 `include "define.vh"
-// fields of instruction register
-/*`define oper_type IR[31:27]
-`define rdst IR[26:22]
-`define rsrc1 IR[21:17]
-`define imm_mode IR[16]
-`define rsrc2 IR[15:11]
-`define isrc IR[15:0]
-
-//operations
-`define movsgpr 5'b00000
-`define mov 5'b00001
-`define add 5'b00010
-`define sub 5'b00011
-`define mul 5'b00100
-*/
 
 module top(
 input clk, sys_rst,
@@ -25,7 +8,7 @@ input [15:0] din,
 output reg [15:0] dout
 );
 
-
+// register file
 reg [31:0] IR;
 reg [15:0] GPR [31:0];
 reg [15:0] SGPR;
@@ -39,9 +22,9 @@ reg [31:0] data_mem [15:0];     /// data memory
 reg jmp_flag =0;
 reg stop =0;
 
+// alu logic 
 task decode_inst();
 begin
-
 
 case(`oper_type)
 `movsgpr:begin
@@ -155,8 +138,8 @@ end
 end
 */
 
-///// jump and branch
-
+// Control logic
+// jump and branch
 `jump:begin
     jmp_flag=1'b1;
 end
